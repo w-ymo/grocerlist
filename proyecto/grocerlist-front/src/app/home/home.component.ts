9b1c69d68f62f86bab5c628fd079ec58a1914a49
Noel Prieto <noel.prieto.uva@gmail.com>
2024-10-17 00:51:19 +0200

    Pantalla de inicio

--- proyecto/grocerlist-front/src/app/home/home.component.ts
+++ proyecto/grocerlist-front/src/app/home/home.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderGrocerlistComponent } from '../header-grocerlist/header-grocerlist.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderGrocerlistComponent],
+  imports: [HeaderGrocerlistComponent, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 
 export class HomeComponent {
-  
+    public email: String = "";
+
+    constructor(){
+      this.email = "";
+    }
 }
