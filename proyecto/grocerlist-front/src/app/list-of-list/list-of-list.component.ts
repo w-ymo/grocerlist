04509259972d965d2a2dd7077faef70f9ec7c370
Noel Prieto <noel.prieto.uva@gmail.com>
2024-10-28 20:01:06 +0100

    Listas y estilos

--- proyecto/grocerlist-front/src/app/list-of-list/list-of-list.component.ts
+++ proyecto/grocerlist-front/src/app/list-of-list/list-of-list.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderGrocerlistComponent } from '../header-grocerlist/header-grocerlist.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-of-list',
   standalone: true,
-  imports: [HeaderGrocerlistComponent, SideMenuComponent],
+  imports: [HeaderGrocerlistComponent, SideMenuComponent, CommonModule],
   templateUrl: './list-of-list.component.html',
   styleUrl: './list-of-list.component.scss',
 })
 export class ListOfListComponent {
 
+  lists = ['antonio', 'fernando'];
+  
 }
--- proyecto/grocerlist-front/src/app/side-menu/side-menu.component.ts
+++ proyecto/grocerlist-front/src/app/side-menu/side-menu.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss'
 })
