4111ec98b0ee0aa84f3b4573b063a26b731b277b
Noel Prieto <noel.prieto.uva@gmail.com>
2024-10-28 20:07:02 +0100

    Prueba de servicio con lista estatica en el front

--- proyecto/grocerlist-front/src/app/list-of-list/list-of-list.component.ts
+++ proyecto/grocerlist-front/src/app/list-of-list/list-of-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderGrocerlistComponent } from '../header-grocerlist/header-grocerlist.component';
 import { SideMenuComponent } from '../side-menu/side-menu.component';
 import { CommonModule } from '@angular/common';
+import { ListasService } from '../services/list-service/listas.service';
 
 @Component({
   selector: 'app-list-of-list',
@@ -12,6 +13,12 @@ import { CommonModule } from '@angular/common';
 })
 export class ListOfListComponent {
 
-  lists = ['antonio', 'fernando'];
+  lists:any;
+  private listService = new ListasService();
+
+  ngOnInit(): void {
+    //Aqui recibiria el usuario que ha iniciado sesion
+    this.lists = this.listService.getListas();
+  }
   
 }
--- proyecto/grocerlist-front/src/app/services/list-service/listas.service.ts
+++ proyecto/grocerlist-front/src/app/services/list-service/listas.service.ts
@@ -6,4 +6,8 @@ import { Injectable } from '@angular/core';
 export class ListasService {
 
   constructor() { }
+
+  getListas():any{
+    return ['awiki', 'antonio'];
+  }
 }
