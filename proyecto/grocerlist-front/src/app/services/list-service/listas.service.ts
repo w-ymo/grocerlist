65223c5c949a23dc98912e855dfd6b059e5d2054
Noel Prieto <noel.prieto.uva@gmail.com>
2024-10-31 12:14:02 +0100

    Servicios listas

--- proyecto/grocerlist-front/src/app/services/list-service/listas.service.ts
+++ proyecto/grocerlist-front/src/app/services/list-service/listas.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
+import { User } from '../../models/user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { appConfig, baseURI } from '../../app.config';
+import { Observable } from 'rxjs';
+import { List } from '../../models/list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListasService {
 
-  constructor() { }
+  private apiUrl = baseURI + '/listas';
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic ' + btoa('wymo:wymo')
+   });
 
-  getListas():any{
-    return ['awiki', 'antonio'];
+  constructor(private http: HttpClient) { 
+  }
+
+  getListas(user: User): Observable<List> {
+    return this.http.get<List>(this.apiUrl + '/obtener/' + user.nombreUsuario, {headers: this.headers})
   }
 }
